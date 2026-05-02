0a1b5b6e45e92fa124e789fde9a60abbf5c38770
przemek <przemek.bialkwno@gmail.com>
2026-05-02 11:20:41 +0200

    adding content to  article about type modifiers in typescript

--- src/articles/TypeModifiersEN.tsx
+++ src/articles/TypeModifiersEN.tsx
@@ -112,6 +112,115 @@ type UserKeys = keyof typeof user;`}
       />
       Effect:
       <CodeBlock code={`type UserKeys = "name" | "age";`} />
+      <h2>Type Assertions</h2>
+      <p>
+        Type assertions are used to tell TypeScript that "I know better what
+        type it is". You can do this by using <b>as</b> keyword.
+      </p>
+      <CodeBlock
+        code={`const rawData = ["apple", "kiwi"];
+
+JSON.parse(rawData); // Type: any
+
+JSON.parse(rawData) as string[]; // Type: string[]
+
+`}
+      />
+      <h3>Asserting Caught Error Types</h3>
+      <p>
+        In TypeScript, errors caught in try...catch are typed as <b>unknown</b>,
+        so you need to assert or narrow their type before using them.
+      </p>
+      <CodeBlock
+        code={`try {
+ //code to execute
+} catch (err) {
+  console.log(err.message); //  Error ERR IS UNKNOWN
+}`}
+      />
+      <b>type narrowing: (recommended)</b>
+      <CodeBlock
+        code={`try {
+ //code to execute
+} catch (err) {
+   if (err instanceof Error) {
+    console.log(err.message); // OK
+  }
+}`}
+      />
+      <b>type assertion:</b>
+      <CodeBlock
+        code={`try {
+   //code to execute
+} catch (err) {
+  const error = err as Error;
+  console.log(error.message); // OK
+}`}
+      />
+      <h3>Non-Null Assertions</h3>
+      <p>
+        Some values can be null or undefined, if you are 100% sure it really
+        exists than you can use <b>!</b> operator.
+      </p>
+      <CodeBlock
+        code={`const input = document.querySelector("input")!;
+// without ! - you will get error that input is possibly null
+input.value = "Hello";`}
+      />
+      <h3>Assertion assignability</h3>
+      <p>
+        Some assertions type are forbidden. For example, switching from one
+        primitive to another is not allowed, as primitives have nothing to do
+        with each other:{" "}
+      </p>
+      <CodeBlock
+        code={`let myValue = "Stella!" as number;
+`}
+      />
+      If you absolutely must switch a value from one type to a totally unrelated
+      type, you can use a double type assertion:
+      <CodeBlock
+        code={`let myValueDouble = "1337" as unknown as number; // Ok, but you should avoid it
+`}
+      />
+      <h2>Const Assertions</h2>
+      <p>
+        Const assertions in TypeScript (using <b>as const</b>) make values fully
+        immutable and as literal as possible.
+      </p>
+      <CodeBlock
+        code={`const user = {
+  name: "Tom",
+  age: 25,
+} as const;`}
+      />
+      Now TypeScript treats it as:
+      <CodeBlock
+        code={`{
+  readonly name: "Tom";
+  readonly age: 25;
+}`}
+      />
+      <p>
+        It added <b>readonly</b> and converted values to literal types from{" "}
+        <b>string</b> to "Tom" and from <b>number</b> to 25.
+      </p>
+      <h3>Read-only Objects</h3>
+      <p>
+        Read-only objects in TypeScript are objects whose properties cannot be
+        changed after they are created.
+      </p>
+      <CodeBlock
+        code={`const user = {
+  name: "Tom",
+  age: 25,
+} as const;`}
+      />
+      Now everything becomes read-only:
+      <CodeBlock
+        code={`user.name = "Anna"; // ERROR
+user.age = 30; // ERROR`}
+      />
     </div>
   );
 };
